08da533e1a0e89bd000e341ed286992d10347c00
gpietrop <gloria.pietropolli@gmail.com>
2024-01-18 10:36:31 -0500

    add different loop for different opponent strategy.

--- gp_evolution_c4.py
+++ gp_evolution_c4.py
@@ -7,17 +7,19 @@ import numpy as np
 import yaml
 from jax import random
 import jax
+import jax.numpy as jnp
 
 jax.config.update('jax_platform_name', 'cpu')
-import jax.numpy as jnp
+
 from cgpax.evaluation import evaluate_lgp_genome
 from cgpax.individual import generate_population
 from cgpax.run_utils import update_config_with_env_data, compute_masks, compute_weights_mutation_function, \
     compile_parents_selection, compile_crossover, compile_mutation, compile_survival_selection
 from cgpax.utils import CSVLogger
+
 from c4_gym import Connect4Env  # Make sure to import the correct class
 from tqdm import tqdm
-from policies import GreedyPolicy
+from policies import GreedyPolicy, ImprovedGreedyPolicy
 
 
 def evaluate_genomes(genomes_array: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
@@ -41,12 +43,16 @@ if __name__ == '__main__':
         "n_rows": 20,
         "n_extra_registers": 5,
         "seed": 1,
-        "n_individuals": 500,
+        "n_individuals": 20,
         "solver": "lgp",
         "p_mut_lhs": 0.01,
         "p_mut_rhs": 0.01,
         "p_mut_functions": 0.01,
-        "n_generations": 50,
+        "n_generations": 10,
+        "yellow_strategy": {
+            "greedy": 50,
+            "greedy_improved": 50
+        },
         "selection": {
             "elite_size": 10,
             "type": "tournament",
@@ -66,8 +72,12 @@ if __name__ == '__main__':
     connect4_env = Connect4Env(num_disk_as_reward=True)
     update_config_with_env_data(config, connect4_env)
 
-    # Set the opponent policy
-    # connect4_env.set_opponent_policy(new_policy=GreedyPolicy())
+    # Load the opponent strategy config and calculate the number of gen for each policy
+    yellow_strategy = config["yellow_strategy"]
+
+    n_generations = config["n_generations"]
+    n_greedy_generations = int(n_generations * yellow_strategy["greedy"] / 100)
+    n_greedy_improved_generations = n_generations - n_greedy_generations
 
     # Compute masks and compile various functions for genetic programming
     genome_mask, mutation_mask = compute_masks(config)
@@ -87,7 +97,53 @@ if __name__ == '__main__':
     )
 
     # Evolution loop
-    for _generation in tqdm(range(config["n_generations"]), position=1, desc="Generations"):
+    for _generation in tqdm(range(n_greedy_generations), position=1, desc="Generations (Greedy Policy)"):
+        start_eval = time.process_time()
+
+        # set the opponent policy
+        new_policy = GreedyPolicy()
+        fitnesses, percentages, dead_times = evaluate_genomes(genomes)
+        end_eval = time.process_time()
+        eval_time = end_eval - start_eval
+
+        # Log metrics
+        metrics = {
+            "generation": _generation,
+            "max_fitness": max(fitnesses),
+            "max_percentage": max(percentages),
+            "max_dead_time": max(dead_times),
+            "eval_time": eval_time
+        }
+        print("\n mean fitness", np.mean(fitnesses))
+        csv_logger.log(metrics)
+
+        # Parent selection
+        rnd_key, select_key = random.split(rnd_key, 2)
+        parents = select_parents(genomes, fitnesses, select_key)
+
+        # Compute offspring
+        rnd_key, mutate_key = random.split(rnd_key, 2)
+        mutate_keys = random.split(mutate_key, len(parents))
+        if config.get("crossover", False):
+            parents1, parents2 = jnp.split(parents, 2)
+            rnd_key, *xover_keys = random.split(rnd_key, len(parents1) + 1)
+            offspring1, offspring2 = crossover_genomes(parents1, parents2, jnp.array(xover_keys))
+            new_parents = jnp.concatenate((offspring1, offspring2))
+        else:
+            new_parents = parents
+        offspring_matrix = mutate_genomes(new_parents, mutate_keys)
+        offspring = jnp.reshape(offspring_matrix, (-1, offspring_matrix.shape[-1]))
+
+        # Survival selection
+        rnd_key, survival_key = random.split(rnd_key, 2)
+        survivals = parents if select_survivals is None else select_survivals(genomes, fitnesses, survival_key)
+
+        # Update population
+        assert len(genomes) == len(survivals) + len(offspring)
+        genomes = jnp.concatenate((survivals, offspring))
+
+    for _generation in tqdm(range(n_greedy_improved_generations), position=1,
+                            desc="Generations (Greedy Improved Policy)"):
         start_eval = time.process_time()
 
         # set the opponent policy
@@ -133,8 +189,8 @@ if __name__ == '__main__':
         genomes = jnp.concatenate((survivals, offspring))
 
     # Save final results
-    jnp.save(f"results/{run_name}/genotypes.npy", genomes)
-    jnp.save(f"results/{run_name}/fitnesses.npy", fitnesses)
+    jnp.save(f"results/{run_name}/genotypes.txt", genomes)
+    jnp.save(f"results/{run_name}/fitnesses.txt", fitnesses)
     with open(f"results/{run_name}/config.yaml", "w") as file:
         yaml.dump(config, file)
 
@@ -145,6 +201,3 @@ if __name__ == '__main__':
 
     policy = GreedyPolicy()
     evaluate_lgp_genome(best_genome, config, connect4_env, episode_length=42, new_policy=policy)
-
-
-
