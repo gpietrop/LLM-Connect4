1792d1ca01dcb5ae50e13bbb46f2eace8e31c55a
Gloria Pietropolli <gloria.pietropolli@gmail.com>
2024-11-20 18:04:05 +0100

    add new exp with pop 50.

---
+++ run_tests_31_8B_100.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+n_generations=100
+n_individuals=50
+pol="31_8B"
+
+for seed in {1..30}; do
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 0 --greedy_improved 100 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 0 --greedy_improved 90 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 25 --greedy_intermediate 0 --greedy_improved 75 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 50 --greedy_intermediate 0 --greedy_improved 50 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 10 --greedy_improved 90 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 25 --greedy_improved 75 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 50 --greedy_improved 50 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 10 --greedy_improved 80 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 20 --greedy_improved 70 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 33 --greedy_intermediate 33 --greedy_improved 33 --policy_version "$pol"
+        # Add more lines for other combinations if needed
+done
+
