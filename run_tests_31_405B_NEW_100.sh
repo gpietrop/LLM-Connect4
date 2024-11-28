24ab3d437c67d1c9e7cddc8657dd5c445a9724aa
gpietrop <gloria.pietropolli@gmail.com>
2024-11-28 11:25:26 +0100

    change bash file to add expert policy.

--- run_tests_31_405B_NEW_100.sh
+++ run_tests_31_405B_NEW_100.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 n_generations=100
-n_individuals=100
+n_individuals=101
 pol="31_405B_NEW"
 
 for seed in {1..30}; do
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 0 --greedy_improved 100 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 0 --greedy_improved 90 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 25 --greedy_intermediate 0 --greedy_improved 75 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 50 --greedy_intermediate 0 --greedy_improved 50 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 10 --greedy_improved 90 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 25 --greedy_improved 75 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 50 --greedy_improved 50 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 10 --greedy_improved 80 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 20 --greedy_improved 70 --policy_version "$pol"
-        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 33 --greedy_intermediate 33 --greedy_improved 33 --policy_version "$pol"
-        # Add more lines for other combinations if needed
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 0 --greedy_improved 0 --greedy_expert 100 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 10 --greedy_improved 10 --greedy_expert 70 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 20 --greedy_intermediate 20 --greedy_improved 20 --greedy_expert 40 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 20 --greedy_intermediate 0 --greedy_improved 0 --greedy_expert 80 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 20 --greedy_improved 0 --greedy_expert 80 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 0 --greedy_intermediate 0 --greedy_improved 20 --greedy_expert 80 --policy_version "$pol"
+        python gp_evolution_c4.py --seed "$seed" --n_individuals "$n_individuals" --n_generations $n_generations --greedy 10 --greedy_intermediate 20 --greedy_improved 20 --greedy_expert 50 --policy_version "$pol"
 done
-
